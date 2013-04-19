7392020d718a10b2a9459a38bbfaba460155d317
Graham <grahambriggs@gmail.com>
2013-04-19 17:06:07 +0100

    updated queue interface

--- queue.h
+++ queue.h
@@ -2,6 +2,8 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+/* the underlying implementation is a doubly linked list */
+
 struct _queue {
 	struct _queue* next;
 	void* data;
@@ -9,12 +11,26 @@ struct _queue {
 
 typedef struct _queue queue;
 
-extern void init_queue(queue*);
-extern void front(queue*);
-extern void back(queue*);
-extern void push(queue*, void*);
-extern void pop(queue*);
-extern void empty(queue*);
-extern void size(queue*);
+/* init queue - returns 0 on success, -1 on fail */
+extern int init_queue(queue*);
+
+/* gets front element of queue - returns NULL if queue is empty or on failure */
+extern void* front(queue*);
+
+/* gets back element of queue - return NULL if queue is empty or on failure */
+extern void* back(queue*);
+
+/* add element to the rear of the queue - returns 0 on sucess, -1 on failure */
+extern int push(queue*, void*);
+
+/* removes and deletes element from the front of the queue - returns 0 on
+   success, -1 on failure */
+extern int pop(queue*);
+
+/* checks if the queue is empty - returns 0 if empty, 1 not empty */
+extern int empty(queue*);
+
+/* returns the size of the queue, 0 if empty */
+extern size_t size(queue*);
 
 #endif
