d694959b70b64159255f344c5ca6f226dea46958
graybriggs <grahambriggs@gmail.com>
2014-02-02 21:37:09 +0000

    binary trees? YEAAAAAAHHH please

--- tree.h
+++ tree.h
@@ -1,7 +1,22 @@
 #ifndef TREE_H
 #define TREE_H
 
+typedef struct _node {
+	struct _node* left;
+	struct _node* right;
+	void* data;
+} node;
 
+typedef struct _tree {
+	struct _node* root;
+	// other data here
+} tree;
+
+int tree_init(tree*);
+int tree_insert(tree*, const void*);
+node* tree_search(tree*, void*);
+void tree_remove(tree*, void*);
+void tree_destory(tree*);
 
 
 #endif // TREE_H
