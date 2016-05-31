6c17e7c6ca273aa1fdbfc8c3b4c8ed7e99678a32
Graham <grahambriggs@gmail.com>
2016-05-31 20:18:43 +0100

    delete pq

---
+++ priority_queue.c
@@ -0,0 +1,29 @@
+
+
+#include "priority_queue.h"
+
+err_code pq_create(pq* pri_q) {
+
+
+}
+
+err_code pq_delete(pq* pri_q) {
+
+}
+
+err_code pq_insert(pq* pri_q, void* data) {
+
+
+}
+
+void* pq_top(pq* pri_q) {
+
+
+  return NULL:
+}
+
+void* remove_top(pq* pri_q) {
+
+
+  return NULL;
+}
--- priority_queue.h
+++ priority_queue.h
@@ -12,6 +12,7 @@ struct _priority_queue {
 typedef struct _priority_queue pq;
 
 err_code pq_create(pq*);
+void pq_delete(pq*);
 err_code pq_insert(pq*, void* data);
 void* pq_top(pq*);
 void* remove_top(pq*);
